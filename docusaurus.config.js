1e6d728031b573cd5184daab25c611b41287b7ad
Diego-Palomino-Jjjeeee <palominodiego210@gmail.com>
2024-03-22 17:30:47 -0500

    Correccion de nombre de usuario

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://Diego-Palomino-Jjjeeee.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/documentacion',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Diego-Palomino-Jjjeeee', // Usually your GitHub org/user name.
+  projectName: 'documentacion', // Usually your repo name.
+  deploymentBranch:'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
